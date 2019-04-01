40f848f756da9188a314a1c134af501cec6182ec
viaaprillya <31683677+viaaprillya@users.noreply.github.com>
2019-04-01 10:22:46 +0700

    Add files via upload
    Admin obat dan admin pertolongan pertama

---
+++ module/module/input-obat.php
@@ -0,0 +1,85 @@
+<div class="breadcome-area">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<div class="breadcome-list">
+					<div class="row">
+						<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+							<div class="breadcome-heading">
+
+							</div>
+						</div>
+						<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+							<ul class="breadcome-menu">
+								<li><a href="?module=dashboard">Home</a> <span class="bread-slash">/</span>
+								</li>
+								<li><span class="bread-blod">Dashboard</span>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+    <center><h2>Input Obat</h2></center>
+</div>
+
+<div class="analytics-sparkle-area">
+	<div class="container-fluid">
+		<br>
+        <form>
+                <div class="row">
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label>Nama Obat</label>
+                                    <input type="text" class="form-control" name="upload" required>
+                                </div>
+                            </div>
+                        </div>
+                <div class="row">
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label>Gambar Obat</label>
+                                    <input type="file" class="form-control" name="upload" required>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label>Dosis</label>
+                                    <input type="text" class="form-control" name="Nama" required>
+                                </div>
+                            </div>
+
+                            <div class="col-md-12">
+                                <div class="form-group">
+                                    <label>Penyakit</label>
+                                    <input class="form-control" type="text" name="Isi"required></input>
+                                </div>
+                            </div>
+                            <div class="col-md-12">
+                                <div class="form-group">
+                                    <label>Bahan Aktif</label>
+                                    <input class="form-control" type="text" name="Isi"required></input>
+                                </div>
+                            </div>
+                            <div class="col-md-12">
+                                <div class="form-group">
+                                    <label>Efek Samping</label>
+                                    <input class="form-control" type="text" name="Isi"required></input>
+                                </div>
+                            </div>
+                        </div>
+
+            <div class="col-md-12" align="center">
+                        <button type="submit" class="btn btn-default btn-lg" name="addEvent">Submit</button>
+                    </div>
+        </form>
+			
+		</div>
+    </div>
+
+		<br><br>
+
