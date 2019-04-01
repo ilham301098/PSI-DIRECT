919887c0d815bd545ac211c3d87195e9ff44eb38
claratheodora <31849141+claratheodora@users.noreply.github.com>
2019-04-01 11:23:37 +0700

    Add files via upload
    tampilan donasi

--- donasi.php
+++ donasi.php
@@ -18,9 +18,81 @@ include('front-end/head.php');
         </div><!-- .container -->
     </div><!-- .page-header -->
 
-    <div class="news-wrap">
-        
-    </div>
+    <div class="featured-cause">
+        <div class="container">
+            <div class="row">
+                <div class="col-12" >
+                    <div class="section-heading">
+                        <h2 class="entry-title">Siap memberi bantuan?</h2>
+                    </div><!-- .section-heading -->
+                </div><!-- .col -->
+            </div><!-- .row -->
+
+            <div class="row">
+                <div class="col-12 col-lg-6">
+                    <div class="cause-wrap d-flex flex-wrap justify-content-between">
+                        <figure class="m-0">
+                            <img src="images/bencana.jpg" alt="">
+                        </figure>
+
+                        <div class="">
+                            <header class="entry-header d-flex flex-wrap align-items-center">
+                                <h3 class="entry-title w-100 m-0"><a href="#">Bantu Korban Bencana Banjir Bandang Sulsel</a></h3>
+
+                                <div>
+                                    <a href="#">Mar 03, 2019 &nbsp|</a>
+                                </div><!-- .posted-date -->
+
+                                <div>
+                                    <a href="#">&nbsp Ball Room New York</a>
+                                </div><!-- .cats-links -->
+                            </header><!-- .entry-header -->
+
+                            <div class="entry-content">
+                                <p class="m-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris tempus vestib ulum mauris. Lorem ipsum dolor sit amet, consectetur.</p>
+                            </div><!-- .entry-content -->
+
+                            <div class="entry-footer mt-5">
+                                <a href="#" class="btn gradient-bg mr-2">Donate Now</a>
+                            </div><!-- .entry-footer -->
+                        </div><!-- .cause-content-wrap -->
+
+                        
+                    </div><!-- .cause-wrap -->
+                </div><!-- .col -->
+
+                <div class="col-12 col-lg-6">
+                    <div class="cause-wrap d-flex flex-wrap justify-content-between">
+                        <figure class="m-0">
+                            <img src="images/bencana1.jpg" alt="">
+                        </figure>
+
+                        <div class="">
+                            <header class="entry-header d-flex flex-wrap align-items-center">
+                                <h3 class="entry-title w-100 m-0"><a href="#">Bantu Korban Bencana Banjir Bandang Papua</a></h3>
+
+                                <div class="posted-date">
+                                    <a href="#">Aug 25, 2018 &nbsp|&nbsp</a>
+                                </div><!-- .posted-date -->
+
+                                <div class="cats-links">
+                                    <a href="#">Ball Room New York</a>
+                                </div><!-- .cats-links -->
+                            </header><!-- .entry-header -->
+
+                            <div class="entry-content">
+                                <p class="m-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris tempus vestib ulum mauris. Lorem ipsum dolor sit amet, consectetur.</p>
+                            </div><!-- .entry-content -->
+
+                            <div class="entry-footer mt-5">
+                                <a href="#" class="btn gradient-bg mr-2">Donate Now</a>
+                            </div><!-- .entry-footer -->
+                        </div><!-- .cause-content-wrap -->
+                    </div><!-- .cause-wrap -->
+                </div><!-- .col -->
+            </div><!-- .row -->
+        </div><!-- .container -->
+    </div><!-- .featured-cause -->
 
     <?php
     include('front-end/footer.php');
