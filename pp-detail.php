49a28be8fd893d57e4854877e804de244f0856ec
firdaa <49114561+firdaa@users.noreply.github.com>
2019-04-01 19:38:02 +0700

    Halaman Detail Pertolongan Pertama

---
+++ pp-detail.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+include('front-end/head.php');
+?>
+<body class="single-page news-page">
+    <?php
+    include('front-end/navigation.php');
+    ?><!-- .site-header -->
+
+    <div class="page-header">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <center><h1>Informasi Pertolongan Pertama</h1></center>
+                </div><!-- .col -->
+            </div><!-- .row -->
+        </div><!-- .container -->
+    </div><!-- .page-header -->
+
+    <div>
+    <br>
+    <center><h2>Pertolongan Pertama pada Asma</h2></center>
+    <table align = "center">
+    <tr><td align = "justify" width = "900px">Asma disebabkan oleh inflamasi dan sumbatan di saluran bronkioli, yaitu saluran yang membantu paru-paru menarik dan mengembuskan napas. Pada tahun 2009, American Academy of Asthma, Allergy and Immunology menyatakan bahwa satu dari 12 orang di Amerika Serikat didiagnosis menderita asma, dibandingkan dengan 1 banding 12 di tahun 2001. Selama serangan asma, otot-otot di sekitar saluran bronkioli mengetat dan bengkak sehingga membuat jalan udara menyempit dan menyebabkan orang sulit bernapas. Pemicu serangan asma yang umum antara lain paparan terhadap penyebab alergi (seperti rumput, rambut, serbuk sari, dsb.), penyebab iritasi di udara (seperti asap atau aroma kuat), penyakit (seperti flu), stres, kondisi cuaca ekstrem (seperti panas ekstrem), atau aktivitas fisik dan olahraga. Pengetahuan akan tanda-tanda serangan asma dan apa yang harus dilakukan saat serangan terjadi dapat membantu menyelamatkan jiwa seseorang.</td></tr>
+
+    <tr><td align="justify" width = "900px"><ol>
+        <center><img src = components/images/asma1.jpg></center>
+        <li><b>Bantulah penderita menemukan posisi nyaman.</b> Kebanyakan orang yang menderita asma akan lebih nyaman dengan duduk daripada berdiri atau berbaring. Tegakkan badannya untuk membantu pengembangan paru-paru dan mempermudah pernapasan. Biarkan ia mencondongkan tubuh sedikit ke arah Anda atau kursi untuk dukungan. Beberapa penderita asma bisa duduk dalam posisi tripod, yaitu mencondongkan tubuh ke depan dengan tangan di atas lutut untuk meringankan tekanan di diafragma.
+        Asma dapat diperburuk oleh kecemasan, tetapi tidak dipicu kecemasan. Artinya, selama serangan, penderita akan merespons lebih cepat bila ia tenang. Kecemasan melepaskan kortisol di tubuh yang menyempitkan bronkioli, yaitu saluran yang mengalirkan udara melalui hidung dan/atau mulut ke kantung udara di paru-paru.
+        Anda harus tetap tenang dan meyakinkan karena itu penting untuk membantu si penderita mempertahankan ketenangannya.</li>
+        <center><img src = components/images/asma2.jpg></center>
+        <li><b>Tanyakan dengan tenang, “Apakah kau punya asma?”</b> Walaupun ia tidak dapat menjawab secara verbal karena sesak atau batuk, mungkin ia akan mengangguk atau menunjuk tempat inhaler atau kartu petunjuknya berada.
+        Tanyakan apakah ia memiliki rencana tindakan darurat asma. Saat ini, banyak orang yang bersiap untuk serangan asma telah membawa rencana darurat yang tertulis. Jika ia punya, ambil dan bantulah ia mengikuti rencana tersebut.</li>
+        .<br>.<br>.<br>
+    </ol>
+    <i>Sumber : id.wikihow.com/Mengatasi-Serangan-Asma</i>
+</td></tr>
+    </div>
+    </table>
+    <br>
+
+</body>
+</html>
