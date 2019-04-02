ca923e8bbe1be71aaa96b3a41986bdd76e776883
ilham301098 <ilham301098@gmail.com>
2019-04-02 22:42:53 +0700

    Update newartikel.php

--- module/module/newartikel.php
+++ module/module/newartikel.php
@@ -1,30 +1,32 @@
 <style>
-table {
-  font-family: arial, sans-serif;
-  border-collapse: collapse;
-  width: 100%;
-}
+	table {
+		font-family: arial, sans-serif;
+		border-collapse: collapse;
+		width: 100%;
+	}
 
-td, th {
-  border: 1px solid #dddddd;
-  text-align: left;
-  padding: 8px;
-}
-tr:nth-child(even) {
-  background-color: #dddddd;
-}
-button {
-  background-color: orange;
-  border: none;
-  color: white;
-  padding: 15px 25px;
-  text-align: center;
-  font-size: 16px;
-  cursor: pointer;
-}
-button:hover {
-  background-color: blue;}
+	td, th {
+		border: 1px solid #dddddd;
+		text-align: left;
+		padding: 8px;
+	}
+	tr:nth-child(even) {
+		background-color: #dddddd;
+	}
+	button {
+		background-color: orange;
+		border: none;
+		color: white;
+		padding: 15px 25px;
+		text-align: center;
+		font-size: 16px;
+		cursor: pointer;
+	}
+	button:hover {
+		background-color: blue;
+	}
 </style>
+
 <div class="breadcome-area">
 	<div class="container-fluid">
 		<div class="row">
@@ -38,14 +40,20 @@ button:hover {
 						</div>
 						<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
 							<ul class="breadcome-menu">
-								<li><a href="?module=dashboard">Home</a> <span class="bread-slash">/</span>
+								<li>
+									<a href="?module=dashboard">Home</a> 
+									<span class="bread-slash">/</span>
 								</li>
-								<li><span class="bread-blod">Dashboard</span> <span class="bread-slash">/</span>
+								<li>
+									<span class="bread-blod">Dashboard</span> 
+									<span class="bread-slash">/</span>
 								</li>
-                <li><span class="bread-blod">Siaga Bencana</span> <span class="bread-slash">/</span>
-                </li>
-                  <li><a href="?module=artikeladm">Artikel</span>
-  								</li>
+								<li>
+									<span class="bread-blod">Siaga Bencana</span> 
+									<span class="bread-slash">/</span>
+								</li>
+								<li>
+									<a href="?module=artikeladm">Artikel</a>
 								</li>
 							</ul>
 						</div>
@@ -55,58 +63,75 @@ button:hover {
 		</div>
 	</div>
 </div>
-<section id="main-content">
-  <section class="wrapper">
-    <!-- BASIC FORM VALIDATION -->
-    <div class="row mt">
-      <div class="col-lg-12">
-        <h4>Form Artikel Baru</h4>
-        <div class="form-panel">
-          <form role="form" class="form-horizontal style-form">
-            <div class="form-group has-error">
-              <label class="col-lg-2 control-label">Judul Artikel</label>
-              <div class="col-lg-10">
-                <input type="text" placeholder="" id="f-name" class="form-control">
-                <!-- <p class="help-block">Successfully done</p> -->
-              </div>
-            </div>
-            <div class="form-group has-error">
-              <label class="col-lg-2 control-label">Sumber Artikel</label>
-              <div class="col-lg-10">
-                <input type="text" placeholder="" id="l-name" class="form-control">
-                <!-- <p class="help-block">Aha you gave a wrong info</p> -->
-              </div>
-            </div>
-            <div class="form-group has-error">
-              <label class="col-lg-2 control-label">Deskripsi</label>
-              <div class="col-lg-10">
-                <input type="textarea" placeholder="" id="email2" class="form-control">
-                <!-- <p class="help-block">Something went wrong</p> -->
-              </div>
-            </div>
-            <div class="form-group has-error">
-              <label class="col-lg-2 control-label">Deskripsi</label>
-              <div class="col-lg-10">
-                <input type="file" placeholder="" id="email2" class="form-control">                <!-- <p class="help-block">Something went wrong</p> -->
-              </div>
-            </div>
-            <div class="form-group">
-              <div class="col-lg-offset-2 col-lg-10">
-                <button class="btn btn-theme" type="submit">Save</button>
-                <button class="btn btn-theme" type="submit">Cancel</button>
-              </div>
-            </div>
-          </form>
-        </div>
-        <!-- /form-panel -->
-      </div>
-      <!-- /col-lg-12 -->
-    </div>
-    <!-- /row -->
-    <!-- FORM VALIDATION -->
 
-<div class="analytics-sparkle-area">
-	<div class="container-fluid">
+<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+	<div class="sparkline12-list mt-b-30">
+		<div class="sparkline12-hd">
+			<div class="main-sparkline12-hd">
+				<h1 align="center">Form Tambah Artikel</h1>
+			</div>
 		</div>
-  </div>
+		<div class="sparkline12-graph">
+			<div class="input-knob-dial-wrap">
+				<div class="row">
+					<div class="col-lg-2 col-md-2 col-sm-2 col-xs-12">
+						<div class="input-mask-title">
+							<label>Judul Artikel</label>
+						</div>
+					</div>
+					<div class="col-lg-10 col-md-10 col-sm-10 col-xs-12">
+						<div class="input-mark-inner mg-b-22">
+							<input type="text" class="form-control" placeholder="">
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-lg-2 col-md-2 col-sm-2 col-xs-12">
+						<div class="input-mask-title">
+							<label>Sumber Artikel</label>
+						</div>
+					</div>
+					<div class="col-lg-10 col-md-10 col-sm-10 col-xs-12">
+						<div class="input-mark-inner mg-b-22">
+							<input type="text" class="form-control" placeholder="">
+
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-lg-2 col-md-2 col-sm-2 col-xs-12">
+						<div class="input-mask-title">
+							<label>Deskripsi</label>
+						</div>
+					</div>
+					<div class="col-lg-10 col-md-10 col-sm-10 col-xs-12">
+						<div class="input-mark-inner mg-b-22">
+							<input type="text" class="form-control" placeholder="">
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-lg-2 col-md-2 col-sm-2 col-xs-12">
+						<div class="input-mask-title">
+							<label>Foto</label>
+						</div>
+					</div>
+					<div class="col-lg-10 col-md-10 col-sm-10 col-xs-12">
+						<div class="input-mark-inner mg-b-22">
+							<input type="file" placeholder="" id="email2" class="form-control">   
+						</div>
+					</div>
+				</div>
+				<br><br>
+				<div class="row" align="center">
+							<button class="btn btn-theme" type="submit">Save</button>
+							<button class="btn btn-theme" type="submit">Cancel</button>
+						
+				</div>
+				
+				
+			</div>
+		</div>
+	</div>
 </div>
+
