d6dbf4cb274876bea41ec49068396d83b477d993
ilham301098 <ilham301098@gmail.com>
2019-04-02 22:29:36 +0700

    Update artikeladm.php (Fix head)

--- module/module/artikeladm.php
+++ module/module/artikeladm.php
@@ -1,6 +1,3 @@
-<head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <style>
 body {
   font-family: Arial;
@@ -71,7 +68,6 @@ button:hover {
 
 
 </style>
-</head>
 
 <div class="breadcome-area">
 	<div class="container-fluid">
