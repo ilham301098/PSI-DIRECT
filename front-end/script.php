0f669326538752fefa73f89ab9b902d781934b8c
Arinal <31654971+arinalizzay@users.noreply.github.com>
2019-05-07 04:51:21 +0700

    jquery2 + datatables

--- front-end/script.php
+++ front-end/script.php
@@ -1,4 +1,4 @@
-<script type='text/javascript' src='components/js/jquery.js'></script>
+<script type='text/javascript' src='components/js/jquery2.js'></script>
 <script type='text/javascript' src='components/js/jquery.collapsible.min.js'></script>
 <script type='text/javascript' src='components/js/swiper.min.js'></script>
 <script type='text/javascript' src='components/js/jquery.countdown.min.js'></script>
@@ -6,6 +6,9 @@
 <script type='text/javascript' src='components/js/jquery.countTo.min.js'></script>
 <script type='text/javascript' src='components/js/jquery.barfiller.js'></script>
 <script type='text/javascript' src='components/js/custom.js'></script>
+<script type='text/javascript' src='components/js/jquery.dataTables.min.js'></script>
+<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.js"></script>
 <script src="back-end/js/sparkline/jquery.sparkline.min.js"></script>
 <script src="back-end/js/sparkline/sparkline-active.js"></script>
+<script src="back-end/js/tawk-chat.js"></script>
 <script src="back-end/js/main.js"></script>
