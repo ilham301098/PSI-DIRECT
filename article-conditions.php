0db95ba4bdc376df8c1d2f9bbc4835ecf16973e3
mauliamutiaa <48859992+mauliamutiaa@users.noreply.github.com>
2019-05-17 12:08:30 +0700

    add halaman post-disaster conditions

---
+++ article-conditions.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+include('front-end/head.php');
+?>
+<body class="single-page causes-page">
+    <?php
+    include('front-end/navigation.php');
+    ?><!-- .site-header -->
+
+    <div class="page-header">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h1 align="center">Pemulihan</h1>
+                </div><!-- .col -->
+            </div><!-- .row -->
+        </div><!-- .container -->
+    </div><!-- .page-header -->
+
+    <div class="featured-cause">
+        <div class="container">
+            <div class="col-12 col-md-6 col-lg-4 mt-4 mt-lg-0">
+                <a href="article-conditions.php">
+                <div class="icon-box active">
+                    <figure class="d-flex justify-content-center">
+                        <img src="components/images/hands-gray.png" alt="">
+                        <img src="components/images/hands-white.png" alt="">
+                    </figure>
+
+                    <header class="entry-header">
+                        <h3 class="entry-title">Post-Disaster Conditions</h3>
+                    </header>
+                </div><!--icon box-->
+                <!--<div class="entry-footer mt-5">
+                    <figure class="d-flex justify-content-center">
+                        <img src="components/images/hands-gray.png" alt="">
+                        <img src="components/images/hands-white.png" alt="">
+                    </figure>
+
+                    <a href="detail-article-pemulihan.php" class="btn gradient-bg mr-2">Read More</a>
+                </div><!- .entry-footer -->
+            </div><!--col--> 
+
+    <br>
+
+            <div class="row">
+                <div class="col-8">
+                    <div class="search-widget">
+                        <form class="flex flex-wrap align-items-center">
+                            <input type="search" placeholder="Search...">
+                            <button type="submit" class="flex justify-content-center align-items-center">GO</button>
+                        </form><!-- .flex -->
+                    </div><!-- .search-widget -->
+                </div><!-- .col -->
+            </div><!-- .row -->
+
+            <div class="row">
+                <div class="col-12 col-lg-6">
+                    <div class="cause-wrap d-flex flex-wrap justify-content-between">
+                        <figure class="m-0">
+                            <img src="components/images/skata-1539341071.jpg" alt="">
+                        </figure>
+
+                        <div class="cause-content-wrap">
+                            <header class="entry-header d-flex flex-wrap align-items-center">
+                                <h3 class="entry-title w-100 m-0"><a href="#">7 Cara Memulihkan Trauma Paska Bencana</a></h3>
+
+                                <div class="posted-date">
+                                    <a href="#">March 12, 2018 </a>
+                                </div><!-- .posted-date -->
+
+                                <div class="cats-links">
+                                    <a href="#">by Dokter Muda Pricilla Frinka</a>
+                                </div><!-- .cats-links -->
+                            </header><!-- .entry-header -->
+
+                            <div class="entry-content">
+                                <p class="m-0">Beberapa hari lalu gempa dan tsunami yang mengguncang Palu-Donggala, Sulawesi Tengah telah mengakibatkan banyak kerusakan dan menelan puluhan korban jiwa. Mereka yang terdampak bencana gempa mengungsi ke tempat-tempat aman.</p>
+                            </div><!-- .entry-content -->
+
+                            <div class="entry-footer mt-5">
+                                <a href="detail-article-conditions.php" class="btn gradient-bg mr-2">Read More</a>
+                            </div><!-- .entry-footer -->
+                        </div><!-- .cause-content-wrap -->
+                    </div><!-- .cause-wrap -->
+                </div><!-- .col -->
+
+                   
+                </div><!-- .row -->
+                <ul class="pagination d-flex flex-wrap align-items-center p-0">
+                    <li class="active"><a href="#">01</a></li>
+                    <li><a href="#">02</a></li>
+                    <li><a href="#">03</a></li>
+                </ul>
+            </div><!-- .container -->
+        </div>
+    </div>
+
+    <?php
+    include('front-end/footer.php');
+    include('front-end/script.php');
+    ?>
+
+</body>
+</html>
