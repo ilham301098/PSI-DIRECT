8d206fefe0243718a0d09bca46b64b39ddf91a61
mauliamutiaa <48859992+mauliamutiaa@users.noreply.github.com>
2019-05-17 12:11:19 +0700

    add halaman artikel post-disaster conditions

---
+++ detail-article-conditions.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+include('front-end/head.php');
+?>
+<body class="single-page news-page">
+    <?php
+    include('front-end/navigation.php');
+    ?><!-- .site-header -->
+
+    <div class="page-header">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h1 align="center">Pemulihan</h1>
+                </div><!-- .col -->
+            </div><!-- .row -->
+        </div><!-- .container -->
+    </div><!-- .page-header -->
+
+    <div class="news-wrap">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-md-6 col-lg-4 mt-4 mt-lg-0">
+                    <a href="article-conditions.php">
+                    <div class="icon-box active">
+                        <figure class="d-flex justify-content-center">
+                            <img src="components/images//hands-gray.png" alt="">
+                            <img src="components/images//hands-white.png" alt="">
+                        </figure>
+
+                        <header class="entry-header">
+                            <h3 class="entry-title">Post-Disaster Conditions</h3>
+                        </header>
+                    </div>
+                </div>
+            </div>
+
+            <br>
+            
+            <div class="row">
+                <div class="col-12 col-lg-8">
+                    <header class="entry-header d-flex flex-wrap justify-content-between align-items-center">
+                        <div class="header-elements">
+                            <div class="posted-date">March 12, 2018</div>
+
+                            <h2 class="entry-title"><a href="#">7 Cara Memulihkan Trauma Paska Bencana</a></h2>
+
+                            <div class="post-metas d-flex flex-wrap align-items-center">
+                                <span class="cat-links">in <a href="#">Causes</a></span>
+                                <span class="post-author">by <a href="#">Dokter Muda Pricilla Frinka</a></span>
+                                <span class="post-comments"><a href="#">3 Comments</a></span>
+                            </div>
+                        </div>
+                    </header>
+
+                <div>
+			    <br>
+			    <a href="#"><img src="components/images//skata-1539341071.jpg" alt=""></a>
+			    <br>
+			    <table align = "center">
+			    <tr>
+			    	<td align = "justify" width = "100px">Beberapa hari lalu gempa dan tsunami yang mengguncang Palu-Donggala, Sulawesi Tengah telah mengakibatkan banyak kerusakan dan menelan puluhan korban jiwa. Mereka yang terdampak bencana gempa mengungsi ke tempat-tempat aman. Bagi para korban dari kejadian ini tentunya dapat menimbulkan trauma yang mendalam ataupun korban biasanya rentan terhadap stres. Gangguan trauma paska stres atau yang dikenal juga sebagai Post Traumatic Stress Disorder (PTSD) adalah sebuah kondisi gangguan kesehatan mental akibat peristiwa yang mengerikan, seperti kecelakaan, perang, ataupun kejadian bencana alam (gempa bumi, tsunami, longsor dan lainnya).</td>
+			    </tr>
+			    <tr>
+			    	<td>Dampak dari gangguan trauma itu sendiri tentu bervariasi, dari yang ringan sampai yang berat. Berikut beberapa diantaranya:</td>
+			    </tr>
+
+			    <tr>
+			    	<td align="justify" width = "900px">
+			    		<ol>
+			        <li>Selalu merasa cemas dan sangat mengganggu</li>
+			        <li>Terbayang-bayang dengan peristiwa bencana</li>
+			        <li>Mimpi buruk yang menyebabkan kesulitan tidur</li>
+			        <li>Kondisi fisik penderita menjadi siaga ketika mereka mengingat ataupun memikirkan trauma yang dialami</li>
+			        .<br>.<br>.<br>
+			    </ol>
+			    <i>Sumber : https://skata.info/article/detail/242/7-cara-memulihkan-trauma-paska-bencana</i>
+			</td>
+		</tr>
+			    </table>
+			    <br>
+</div>
+</div>
+
+                <div class="col-5 col-md-12">
+                    <div class="sidebar">
+
+                        <div class="popular-posts">
+                            <h2>Artikel Terkait</h2>
+
+                            <ul class="p-0">
+                                <li class="d-flex flex-wrap justify-content-between align-items-center">
+                                    <figure><a href="#"><img src="components/images//p-1.jpg" alt=""></a></figure>
+
+                                    <div class="entry-content">
+                                        <h3 class="entry-title"><a href="#">A new cause to help</a></h3>
+
+                                        <div class="posted-date">March 12, 2018</div>
+                                    </div>
+                                </li>
+
+                                <li class="d-flex flex-wrap justify-content-between align-items-center">
+                                    <figure><a href="#"><img src="components/images//p-2.jpg" alt=""></a></figure>
+
+                                    <div class="entry-content">
+                                        <h3 class="entry-title"><a href="#">We love to help people</a></h3>
+
+                                        <div class="posted-date">March 10, 2018</div>
+                                    </div>
+                                </li>
+
+                                <li class="d-flex flex-wrap justify-content-between align-items-center">
+                                    <figure><a href="#"><img src="components/images//p-3.jpg" alt=""></a></figure>
+
+                                    <div class="entry-content">
+                                        <h3 class="entry-title"><a href="#">The new ideas for helping</a></h3>
+
+                                        <div class="posted-date">March 09, 2018</div>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div><!-- .cat-links -->
+                    </div><!-- .sidebar -->
+                </div><!-- .col -->
+            </div>
+        </div>
+
+        <br>
+        <br>
+
+    <?php
+    include('front-end/footer.php');
+    include('front-end/script.php');
+    ?>
+
+</body>
+</html>
