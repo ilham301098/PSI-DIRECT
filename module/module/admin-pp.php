0cbe7ee24b03f8a82bb057985400c48050266311
farissaR <31720610+farissaR@users.noreply.github.com>
2019-05-21 14:06:19 +0700

    fungsi display isi db pp dihalaman admin

--- module/module/admin-pp.php
+++ module/module/admin-pp.php
@@ -47,28 +47,100 @@
                                     </div>
                                     </div>
                                     <br>
-    <div id=tabel-obat>
-    <center>
-    <table border="1" bordercolor="#d9d9d9">
+<?php
+require('config/db.php');
+$sql="";
+$sql = 'SELECT * FROM pertolongan_pertama ORDER by id ASC';
+$result = mysqli_query($con,$sql);
+$no = 1;
+$output = '
+<table class="table table-bordered table-striped">
+  <tr>
+    <th width="2%">No</th>
+    <th width="10%">JUDUL</th>
+    <th width="45%">DESKRIPSI KONTEN</th>
+    <th width="10%">Change</th>
+    <th width="10%">Remove</th>
+</tr>
+';
+while($row = mysqli_fetch_array($result))
+{
+    $output .= '
     <tr>
-        <td width ="40px" align = "center">No</td>
-        <td width ="350px" align = "center">Judul</td>
-        <td width ="350px" align = "center">Aksi</td>
-    </tr>
-    <tr>
-        <td width ="40px" align = "center">1</td>
-        <td width ="150px" align = "center">Pertolongan Pertama pada Asma</td>
-         <td width ="200px" height ="50px" align = "center"><button type="button" class="btn btn-custon-four btn-default"><a href="?module=edit-pp">Edit</a></button> <button type="button" class="btn btn-custon-four btn-default" data-toggle="modal" data-target=".bs-modal-DeleteArticle">Delete</button><button type="button" class="btn btn-custon-four btn-default" data><a href="pp-detail.php">Pratinjau</a></button></td>
-    </tr>
-    <tr>
-        <td width ="40px" align = "center">2</td>
-        <td width ="150px" align = "center">Pertolongan Pertama pada Diare</td>
-         <td width ="200px" height ="50px" align = "center"><button type="button" class="btn btn-custon-four btn-default">Edit</button> <button type="button" class="btn btn-custon-four btn-default">Delete</button> <button type="button" class="btn btn-custon-four btn-default">Pratinjau</button></td>
-    </tr>
-</table>
-<br>
-<a href="#" class="previous">&laquo; Previous </a>
-<a href="#" class="next">Next &raquo;</a>
+      <td>'.$no.'</td>
+      <td>'.$row['judul'].'</td>
+      <td>'.$row['isi'].'</td>
+      <td><button type="button" name="edit" data-toggle="modal" data-target=".bs-modal-ChangeArticle'.$row["id"].'" class="btn btn-custon-four btn-default"  id="'.$row["id"].'">Edit</button></a></td>
+      <td><button type="button" name="delete" data-toggle="modal" data-target=".bs-modal-DeleteArticle'.$row['id'].'" class="btn btn-custon-four btn-default" id="'.$row["id"].'">Delete</button>
+      </td>
+      <!--Delete Item Modal -->
+      <div id="delete<?php echo $id; ?>" class="modal fade bs-modal-DeleteArticle'.$row["id"].'" role="dialog">
+        <div class="modal-dialog">
+          <form method="post">
+            <!-- Modal content-->
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title">Delete</h4>
+            </div>
+            <div class="modal-body">
+                <form action="" method="POST">
+                  <input type="hidden" name="delete_id" value="'.$row["id"].'">
+                  <div class="alert alert-danger">Are you Sure you want to Delete <strong>
+                    '.$row['judul'].'</strong>? </div>
+                    <div class="modal-footer">
+                      <button type="submit" name="delete" class="btn btn-danger"><span class="glyphicon glyphicon-trash"></span> YES</button>
+                      <button type="button" class="btn btn-default" data-dismiss="modal"><span class="glyphicon glyphicon-remove-circle"></span> NO</button>
+                  </div>
+              </form>
+          </div>
+      </div>
+  </form>
+</div>
+</div>
+<!--Edit Item Modal -->
+<div id="edit<?php echo $id; ?>" class="modal fade bs-modal-ChangeArticle'.$row["id"].'" role="dialog">
+  <form method="POST" class="form-horizontal" role="form">
+    <div class="modal-dialog modal-lg">
+      <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4 class="modal-title">Edit Artikel</h4>
+      </div>
+      <div class="modal-body">
+          <input type="hidden" name="edit_id" value="'.$row["id"].'">
+          <div class="form-group">
+            <label class="control-label col-sm-2" for="judul">Judul:</label>
+            <div class="col-sm-4">
+              <input type="text" class="form-control" id="judul" name="judul" value="'.$row['judul'].'" placeholder="Judul" required autofocus> </div>
+              <label class="control-label col-sm-2" for="sumber">Sumber:</label>
+            <div class="form-group">
+                <label class="control-label col-sm-2" for="isi">Description:</label>
+                <div class="col-sm-4">
+                  <textarea class="form-control" id="isi" name="isi" placeholder="Description" required style="width: 100%;">
+                    '.$row['isi'].'
+                </textarea>
+            </div>
+          </div>
+      <div class="modal-footer">
+        <button type="submit" class="btn btn-primary" name="edit"><span class="glyphicon glyphicon-edit"></span> Edit</button>
+        <button type="button" class="btn btn-warning" data-dismiss="modal"><span class="glyphicon glyphicon-remove-circle"></span> Cancel</button>
+    </div>
+</div>
+</div>
+</form>
+</div>
+</tr>
+';
+$no++;
+}
+$output .= '</table>';
+echo $output;
+ ?>
+ <br>
+ <a href="#" class="previous">&laquo; Previous </a>
+ <a href="#" class="next">Next &raquo;</a>
 
 <div class="modal fade bs-modal-DeleteArticle" tabindex="-1" role="dialog" aria-hidden="true">
             <div class="modal-dialog modal-sm">
@@ -95,8 +167,7 @@
 
 </center>
 </div>
-			
+
 		</div>
 
 		<br><br>
-
