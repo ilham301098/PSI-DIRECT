5b9ebbf00b5d71019d1ba9335b944e7d24e3485c
Clara Theodora <theodoraclara@gmail.com>
2019-05-28 20:27:49 +0700

    donasi frontend fix

--- donasi.php
+++ donasi.php
@@ -14,15 +14,16 @@ require('config/db.php');
     $sum = $row['value_sum'];
     $d=strtotime("now");
     $donasi =null;
+    $random = mt_rand(1,99);
 
     if (isset($_POST['kirim'])) {
     // Get data
-        $donasi=$_POST['nominal'];
+        $donasi=$_POST['nominal'] + $random;
         $time = date("Y-m-d h:i:sa", $d);
         $status="Belum Terverifikasi";
         $id = $_SESSION['user_id'];
           
-      $sql = "INSERT INTO donasi (id, donasi, tanggal, status) 
+      $sql = "INSERT INTO donasi (id_user, donasi, tanggal, status) 
       VALUES ('$id','$donasi','$time','$status')";
     // execute query
     mysqli_query($con, $sql);
@@ -41,7 +42,7 @@ require('config/db.php');
     </div><!-- .page-header -->
     <?php if(!isset($_SESSION['signed_in'])){
             //the user is not signed in
-        echo '<br><br><center><h2>Sorry, you have to be <a href="login.php">signed in</a> to create a thread.</h2></center><br><br>';
+        echo '<br><br><center><h2>Maaf, anda harus <a href="login.php">login</a> untuk memberikan donasi.</h2></center><br><br>';
     }
     else{
         echo '
@@ -73,16 +74,25 @@ require('config/db.php');
                             <input type="number" name="nominal" id="user_input" class="form-control" placeholder="Input nominal donasi">
                         </div>
                     </div>
-                    <p><span id="display"></span></p>
                     <br>
+                    <p><span><b>Harap Transfer Nominal Donasi ke :</b></span></p>
+                    <p><span>Rekening BCA: 242342523 a.n. DIRECT</span></p>
+                    <p><span>Rekening BRI: 242342523 a.n. DIRECT</span></p>
+                    <br>
+                    <div class="container">
+                        <p><span><i>Berbagi bukan tentang seberapa besar dan seberapa berharganya hal yang kau beri, namun seberapa tulus dan ikhlasnya apa yang ingin kau beri.</i></span></p>
+                    </div>
+                    <br><br>
                     <div align="center">
                         <button class="btn btn gradient-bg my-1 my-sm-0" type="submit" onclick="showInput()" name="kirim" value="Kirim">Donasi</button>';?>
                         <script>
                             function showInput() {
                             //    document.getElementById('display').innerHTML = 
                             //                document.getElementById("user_input").value;
-                                $b=Number(document.getElementById("user_input").value);
-                               window.alert($b + 23);
+                                a=Number("<?= $random ?>");
+                                b=Number(document.getElementById("user_input").value);
+                                c=a+b
+                               window.alert("Donasi yang harus ditransfer adalah " +c +". Harap perhatikan nominal yang harus di transfer");
                            }
                         </script>
                     <?php echo '</div>
